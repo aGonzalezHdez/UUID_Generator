74aca68edcc9222c7500d7b534fb44221521400b
Torraskes <78329836+Torraskes@users.noreply.github.com>
2025-05-30 16:12:23 -0600

    feat: starting project

---
+++ UUID_Generator/Program.cs
@@ -0,0 +1,13 @@
+﻿using UUID_Generator.Services;
+
+namespace UUID_Generator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var generador = new UUIDGenerator();
+        Console.WriteLine(generador.Generate("Hello, World!") );
+        
+    }
+}
---
+++ UUID_Generator/Services/UUIDGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace UUID_Generator.Services;
+
+public class UUIDGenerator
+{
+    public Guid Generate(string input)
+    {
+        using var sha256 = SHA256.Create();
+        byte[] hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+        // Modificar los bits para que cumpla con UUID versión 4 (como en Python)
+        hash[6] = (byte)((hash[6] & 0x0F) | 0x40); // versión   4
+        hash[8] = (byte)((hash[8] & 0x3F) | 0x80); // variant RFC 4122
+
+        byte[] guidBytes = new byte[16];
+        Array.Copy(hash, guidBytes, 16);
+
+        return new Guid(guidBytes);
+    }
+}
